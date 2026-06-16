4bbeb44dbe0f8c0b31511fd38b6322572d042631
Thanhdatdevv <thanhdate09@gmail.com>
2026-06-16 16:16:26 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "mini-anotepad",
+  "version": "1.0.0",
+  "description": "Simple notepad API",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2"
+  }
+}
+  
